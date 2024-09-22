40d0827c3bff1f403425ae78de10787b19a7a442
Adam Fratino <fratino.a@gmail.com>
2024-09-22 02:11:32 -0400

    fix: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import Image from "next/image";
-import { PropsWithChildren } from "react";
 
 import Photo from "./components/photo";
 import Details from "./components/details";
@@ -9,19 +7,6 @@ export const metadata: Metadata = {
   title: "Adam Fratino",
   description:
     "UX/UI engineer with a background in graphic design, focused on building design systems, scaling component libraries, and all things interactive.",
-};
-
-const Paragraph = ({ children }: PropsWithChildren) => (
-  <p className="text-paragraph">{children}</p>
-);
-
-const ListItem = ({ children }: PropsWithChildren) => (
-  <li className="text-sm italic mb-2 last:mb-0">{children}</li>
-);
-
-const markdownStyles = {
-  p: Paragraph,
-  li: ListItem,
 };
 
 export default async function Home() {
